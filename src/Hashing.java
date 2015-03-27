dd820caff3035f42cf86cca6a2bc983ba4d85add
Emil Christian Lynegaard <ecly@itu.dk>
2015-03-27 19:10:29 +0100

    Had my fun, and finished it.

--- src/Hashing.java
+++ src/Hashing.java
@@ -3,14 +3,25 @@ import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/**
+ * Adds Unique strings to an Array of LinkedLists using Hashing
+ * and Separate-Chaining. Duplicates are not allow by default.
+ */
+
 public class Hashing {
 
     private LinkedList<String>[] list;
     private int M; //size of array
     private int N; //numbers of object in array
+    private final boolean duplicatesAllowed = false;
 
+
+    /**
+     * Create an Array of default size 16
+     * containing LinkedList<String>.
+     */
     public Hashing() {
-        M = 16; //default value
+        M = 16; //default array length
         list = new LinkedList[M];
 
         //fill the array
@@ -19,11 +30,24 @@ public class Hashing {
         }
     }
 
+    /**
+     * Insert a String into the LinkedList
+     * at location of the hash if it doesn't exist already.
+     * Resizes array if needed.
+     * @param s    String to be added
+     */
     public void insert(String s) {
-        checkSize();
-        int hash = Math.abs(s.hashCode() % M);
-        list[hash].add(s); //add the string to list at index of hash % N
+        checkSize(); //check if resizing is needed
+        if (!hashContains(s) || duplicatesAllowed) {
+            N++;
+            int hash = Math.abs(s.hashCode() % M);
+            list[hash].add(s); //add the string to list at index of hash % N
+        }
+    }
 
+    private boolean hashContains(String s){
+        int hash = Math.abs(s.hashCode() % M);
+        return list[hash].contains(s);
     }
 
     private void checkSize() {
@@ -50,7 +74,7 @@ public class Hashing {
         }
     }
 
-    public void makeSmaller() {
+    private void makeSmaller() {
         LinkedList<String>[] aux = list;
         M = M/2; //half M
         list = new LinkedList[M]; //replace list with new bigger list
@@ -67,9 +91,29 @@ public class Hashing {
         }
     }
 
+    /**
+     * Deletes string from the list if it exists
+     * otherwise prints that it doesn't exist in console.
+     * @param s
+     */
+    public void delete(String s) {
+        int hash = Math.abs(s.hashCode() % M);
+        if (list[hash].contains(s))
+            list[hash].remove(s);
+        else
+            System.out.println("The string attempted to delete does not exist");
+    }
+
+
+    /**
+     * Tests the Hash.
+     * Takes an input file of Strings. In my case "res/tale.txt".
+     * Prints out first 10% of LinkedList's contents when done adding.
+     * @param args
+     */
     public static void main(String[] args) {
         String filename = args[0];
-        File file = new File("res/" + filename);
+        File file = new File(filename);
         Hashing hash = new Hashing();
 
         try {
@@ -79,6 +123,17 @@ public class Hashing {
                 String s = scanner.next();
                 hash.insert(s);
             }
+
+            //Prints out content of first 10% of LinkedLists
+            for (int i = 1; i < hash.M/10; i++) {
+                for(String s : hash.list[i])
+                    System.out.println(s + " " + i);
+            }
+
+            System.out.println("N = " + hash.N);
+            System.out.println("M = " + hash.M);
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
